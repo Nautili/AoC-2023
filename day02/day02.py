3ce5f64e227b8b9ed3102414bf73c0fdef2c8d33
nautili <lear.vg@gmail.com>
2023-12-02 07:27:31 -0800

    Cleanup day 2
    Factor out parsing and simplify power/sum functions.

--- day02/day02.py
+++ day02/day02.py
@@ -4,52 +4,58 @@ from operator import mul
 
 def are_valid_cubes(cubes):
     max_cubes = {'red': 12, 'green': 13, 'blue': 14}
-    cube_list = [cube.strip() for cube in cubes.split(',')]
-    for cube in cube_list:
-        count, color = cube.split(' ')
-        if max_cubes[color] < int(count):
-            return False 
-    
-    return True
+    return all(max_cubes[color] >= int(count) for count, color in cubes)
 
 def is_valid_game(game):
-    return all(are_valid_cubes(cubes) for cubes in game.split(';'))
+    return all(are_valid_cubes(cubes) for cubes in game)
         
 def get_valid_sum(games):
-    valid_sum = 0
-    for game_pair in games:
-        game_id, game = game_pair.split(':')
-        if is_valid_game(game):
-            valid_sum += int(game_id.split(' ')[1])
-
-    return valid_sum
+    return sum(game.game_id for game in games if is_valid_game(game.draws))
 
 def get_power(game):
     max_count = {'red': 0, 'green': 0, 'blue': 0}
-    draws = game.split(';')
-
-    for draw in draws:
-        cube_list = [cube.strip() for cube in draw.split(',')]
-        for cube in cube_list:
-            count, color = cube.split(' ')
+    for draw in game.draws:
+        for count, color in draw:
             max_count[color] = max(max_count[color], int(count))
     
     return reduce(mul, max_count.values())
         
 def get_total_power(games):
-    total_power= 0
-    for game_pair in games:
-        _, game = game_pair.split(':')
-        total_power += get_power(game)
+    return sum(get_power(game) for game in games)
 
-    return total_power
+
+class Game:
+    def __init__(self, game_id, draws):
+        self.game_id = game_id
+        self.draws = draws
+
+def parse_games(games):
+    parsed_games = []
+    for game_pair in games:
+        game_id, game = game_pair.split(':')
+       
+        parsed_draws = []
+        draws = game.split(';')     
+        for draw in draws:
+            cube_list = [cube.strip() for cube in draw.split(',')]
+            parsed_cubes = []
+            for cube in cube_list:
+                count, color = cube.split(' ')
+                parsed_cubes.append((int(count), color))
+            parsed_draws.append(parsed_cubes)
+        parsed_games.append(Game(int(game_id.split(' ')[1]), parsed_draws))
+
+    return parsed_games
+
 
 def main():
     with open(sys.argv[1]) as f:
         lines = [line.strip() for line in f.readlines()]
+    
+    games = parse_games(lines)
 
-    print(get_valid_sum(lines))
-    print(get_total_power(lines))
+    print(get_valid_sum(games))
+    print(get_total_power(games))
 
 if __name__ == '__main__':
     main()
