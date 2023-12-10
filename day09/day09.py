19a6e1a4220b9e835bec96f1678734e39838772f
nautili <lear.vg@gmail.com>
2023-12-10 00:11:50 -0800

    Replace get_prev with reversed list
    These are basically the same.

--- day09/day09.py
+++ day09/day09.py
@@ -5,16 +5,11 @@ def get_next(l):
         return 0
     return l[-1] + get_next([n2 - n1 for n1, n2 in zip(l, l[1:])])
 
-def get_prev(l):
-    if all(num == 0 for num in l):
-        return 0
-    return l[0] - get_prev([n2 - n1 for n1, n2 in zip(l, l[1:])])
-
 def main():
     with open(sys.argv[1]) as f:
         lines = [[int(val) for val in line.split()] for line in f.readlines()]
     print(sum(get_next(line) for line in lines))
-    print(sum(get_prev(line) for line in lines))
+    print(sum(get_next(list(reversed(line))) for line in lines))
 
 if __name__ == '__main__':
     main()
