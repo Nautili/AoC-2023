07dd84990108bcc49d25dd13512ddf381cd7cd1a
nautili <lear.vg@gmail.com>
2023-12-20 22:05:20 -0800

    Day 21 - switch to frontier
    This still only solves part 1, but now in a way that should hopefully be
    manageable for part 2.

--- day21/day21.py
+++ day21/day21.py
@@ -1,37 +1,42 @@
 import sys
 from copy import deepcopy
 
-def take_step(grid):
-    new_grid = deepcopy(grid)
-    for row in range(len(grid)):
-        for col in range(len(grid[row])):
-            if new_grid[row][col] == -1:
-                continue
-            
-            dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-            new_grid[row][col] = 0
-            for dir in dirs:
-                cur_row = row + dir[0]
-                cur_col = col + dir[1]
-                if cur_row >= 0 and cur_row < len(grid) and \
-                   cur_col >= 0 and cur_col < len(grid[row]) \
-                   and grid[cur_row][cur_col] > 0:
-                    new_grid[row][col] = 1
-    return new_grid
+class Node:
+    def __init__(self, row, col, prev=[]):
+        self.row = row
+        self.col = col
+        self.prev = prev
+
+def take_step(grid, nodes):
+    new_nodes = {}
+    for node in nodes:
+        for dir in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            cur_row = node.row + dir[0]
+            cur_col = node.col + dir[1]
+            if (cur_row, cur_col) not in node.prev and \
+               cur_row >= 0 and cur_row < len(grid) and \
+               cur_col >= 0 and cur_col < len(grid[0]) \
+               and grid[cur_row][cur_col] != '#':
+                if (cur_row, cur_col) not in new_nodes:
+                    new_nodes[(cur_row, cur_col)] = Node(cur_row, cur_col)
+                new_nodes[(cur_row, cur_col)].prev.append((node.row, node.col))
+    return list(new_nodes.values())
 
 def main():
     with open(sys.argv[1]) as f:
         grid = [list(line.strip()) for line in f.readlines()]
 
-    symbol_map = {'.': 0, 'S': 1, '#': -1}
-    for row in range(len(grid)):
-        for col in range(len(grid[row])):
-            grid[row][col] = symbol_map[grid[row][col]]
+    for r, row in enumerate(grid):
+        for c, col in enumerate(row):
+            if col == 'S':
+                nodes = [Node(r, c)]
 
+    count1 = 0
+    count2 = 1
     for _ in range(64):
-        grid = take_step(grid)
-    position_count = sum([c for row in grid for c in row if c >= 0])
-    print(position_count)
+        nodes = take_step(grid, nodes)
+        count1, count2 = count2, count1 + len(nodes)
+    print(count2)
 
 
 if __name__ == '__main__':
