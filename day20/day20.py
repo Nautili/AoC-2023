3f05d024f6af2f4a2a76e96cf6ca3e05eb4398b9
nautili <lear.vg@gmail.com>
2023-12-20 20:04:07 -0800

    Add part 2
    With a bit of analysis, anything is possible.

--- day20/day20.py
+++ day20/day20.py
@@ -1,6 +1,8 @@
 import sys
 from abc import ABC, abstractmethod
 from collections import deque
+from functools import reduce
+from math import gcd
 
 class Module(ABC):
     def __init__(self, label, outputs):
@@ -30,10 +32,18 @@ class ConjunctionModule(Module):
     def __init__(self, label, outputs, inputs):
         super().__init__(label, outputs)
         self.inputs = {input : 0 for input in inputs}
+        self.toggled = False
 
     def toggle(self, signal, sender):
+        if signal == 1:
+            self.toggled = True
         self.inputs[sender] = signal
         return int(any(input == 0 for input in self.inputs.values()))
+
+    def was_toggled(self):
+        was_toggled = self.toggled
+        self.toggled = False
+        return was_toggled
 
 def get_inputs(lines, label):
     inputs = []
@@ -69,33 +79,50 @@ def build_module(lines):
             module_map[output] = BroadcastModule(output, [])
     return module_map
 
+def press_button(module_map, counts):
+    signal_queue = deque()
+    signal_queue.append((module_map['broadcaster'], 0, None))
+    while signal_queue:
+        module, in_signal, sender = signal_queue.popleft()
+        # debug
+        #print(sender, in_signal, module.label)
+        counts[in_signal] += 1
+
+        out_signal = module.toggle(in_signal, sender)
+        if out_signal != None:
+            for output in module.outputs:
+                signal_queue.append((module_map[output], out_signal, module.label))
+
 def signal_n_times(module_map, n):
     counts = [0, 0]
     for i in range(n):
-        #if i % 1000000 == 0:
-        #    print(i)
-        signal_queue = deque()
-        signal_queue.append((module_map['broadcaster'], 0, None))
-        while signal_queue:
-            module, in_signal, sender = signal_queue.popleft()
-            #print(sender, in_signal, module.label)
-            counts[in_signal] += 1
-            if module.label == 'rx' and in_signal == 0:
-                return i
-
-            out_signal = module.toggle(in_signal, sender)
-            if out_signal != None:
-                for output in module.outputs:
-                    signal_queue.append((module_map[output], out_signal, module.label))
+        press_button(module_map, counts)
     return counts[0] * counts[1]
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+def get_earliest_rx(module_map):
+    for label, module in module_map.items():
+        if 'rx' in module.outputs:
+            target_module = module
+
+    cycle_lengths = []
+    cycle = 1
+    while len(cycle_lengths) < len(target_module.inputs):
+        press_button(module_map, [0, 0])
+        if target_module.was_toggled():
+            cycle_lengths += [cycle]
+        cycle += 1
+
+    return reduce(lcm, cycle_lengths)
 
 def main():
     with open(sys.argv[1]) as f:
         lines = [line.strip() for line in f.readlines()]
 
-    module_map = build_module(lines)
-    #print(signal_n_times(module_map, 1000))
-    print("rx", signal_n_times(module_map, 1000000000000000))
+    print(signal_n_times(build_module(lines), 1000))
+    print(get_earliest_rx(build_module(lines)))
 
 if __name__ == '__main__':
     main()
