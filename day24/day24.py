63e449209a09367a29d9a52360ec5e75aca5efc5
nautili <lear.vg@gmail.com>
2023-12-24 12:45:30 -0800

    Add day 24 part 2
    Time to learn z3.

--- day24/day24.py
+++ day24/day24.py
@@ -1,4 +1,5 @@
 import sys
+import z3
 
 def dot(v1, v2):
     return sum(l * r for l, r in zip(v1, v2))
@@ -33,17 +34,40 @@ def get_intersection(l1, l2):
     is_valid = s >= 0 and t >= 0
     return is_valid, add(p1, mul(v1, s_numerator / denominator))
 
-def get_z_projection(lines):
-    return [[(point[0], point[1], 0), (vec[0], vec[1], 0)] for point, vec in lines]
+def get_projection(lines, mask):
+    new_lines = []
+    for point, vec in lines:
+        new_point = tuple(l * r for l, r in zip(point, mask))
+        new_vec = tuple(l * r for l, r in zip(vec, mask))
+        new_lines.append((new_point, new_vec))
+    return new_lines
 
-def get_in_bounds(lines, lo, hi):
-    in_bounds = 0
+def get_intersections(lines):
+    intersections = set()
     for i in range(len(lines) - 1):
         for j in range(i + 1, len(lines)):
             is_valid, point = get_intersection(lines[i], lines[j])
-            if is_valid and lo <= point[0] <= hi and lo <= point[1] <= hi:
-                in_bounds += 1
-    return in_bounds
+            if is_valid:
+                intersections.add(point)
+    return intersections
+
+def get_in_bounds(lines, lo, hi):
+    points = get_intersections(lines)
+    return sum(lo <= point[0] <= hi and lo <= point[1] <= hi for point in points)
+
+def solve_intersecting_throw(lines):
+    p = z3.RealVector("p", 3)
+    v = z3.RealVector("v", 3)
+    t = z3.RealVector("t", 3)
+
+    s = z3.Solver()
+    for t_i, line in enumerate(lines[:3]):
+        point, vec = line
+        for i in range(3):
+            s.add(p[i] + v[i] * t[t_i] == point[i] + vec[i] * t[t_i])
+
+    if s.check() == z3.sat:
+        return sum(s.model()[val].as_long() for val in p)
 
 def get_lines(lines):
     out_lines = []
@@ -58,10 +82,11 @@ def main():
         lines = [line.strip() for line in f.readlines()]
 
     line_defs = get_lines(lines)
-    orth_proj = get_z_projection(line_defs)
+    orth_proj = get_projection(line_defs, (1, 1, 0))
     low_bound = 200000000000000
     high_bound = 400000000000000
     print(get_in_bounds(orth_proj, low_bound, high_bound))
+    print(solve_intersecting_throw(line_defs))
 
 if __name__ == '__main__':
     main()
